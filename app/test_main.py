2a619cd6e4eeb817e4e9a4e615643b9437976e43
laszloalmasi23 <laszlo.almasi7@gmail.com>
2026-02-22 13:27:15 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expect",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expect: list) -> None:
+    assert (get_human_age(cat_age, dog_age) == expect)
+
+
+def test_et_human_age_negative_inputs() -> None:
+    with pytest.raises((ValueError, TypeError)):
+        get_human_age(-1, -5)
+
+
+def test_et_human_age_wrog_types() -> None:
+    with pytest.raises((TypeError)):
+        get_human_age(3.5, "dsf")
